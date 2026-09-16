6a46be28c34a42f1ae5d1434384f4afb53265658
KenNL42 <geng.2542@gmail.com>
2026-09-16 00:44:17 -0700

    correct info to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // Set the public site URL so the sitemap and canonical URLs are generated correctly.
-  site: 'https://KenNL42.github.io',
+  site: 'https://kennl42.github.io',
   base: '/prasit-website',
   integrations: [
     sitemap({
